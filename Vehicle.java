b63a85818d765189ce8255745721150ff5639269
tomaszwcislo <tomaszwcislo@hotmail.com>
2020-02-26 12:38:19 +0100

    Update code

--- Vehicle.java
+++ Vehicle.java
@@ -1,13 +1,74 @@
 package Vehicle;
 
+import static java.lang.Math.round;
+
 public class Vehicle {
-    String plate; // nazwa pojazdu
-    float capacity; // ładowność
-    int fuelcap; // pojemność zbiornika paliwa
-    double lkm; // spalanie na 100km
-    // użycie metody range() do obliczenia
-    void range() {
-        System.out.println("Pojazd " + plate + " ma " + capacity + "(t) ładowności. Zasięg na jednym tankowaniu wynosi "
-                            + (int) (fuelcap / lkm * 100) + "(km)");
+
+    private String plate;
+    private float capacity;
+    private int fuelcap;
+    private float lkm;
+
+    public Vehicle(String plate, float capacity, int fuelcap, float lkm) {
+        this.plate = plate;
+        this.capacity = capacity;
+        this.fuelcap = fuelcap;
+        this.lkm = lkm;
+    }
+
+    private float calculateRange() {
+        float range = fuelcap / lkm * 100;
+        // zaokrąglanie do 2 miesjc po przecinku
+        range *=100;
+        range = Math.round(range);
+        range /=100;
+        return range;
+    }
+
+    public void printVehicleInfo()  {
+        if (calculateRange() > 1300) {
+            System.out.println("Pojazd " + plate + " ma " + capacity + "(t) ładowności. Zasięg na jednym tankowaniu wynosi " + calculateRange() +
+                    " co wykazuje, że jest bardzo ekonomiczny i należy w niego inwestować.");
+        } else if(calculateRange() > 1200 && calculateRange() < 1300) {
+            System.out.println("Pojazd " + plate + " ma " + capacity + "(t) ładowności. Zasięg na jednym tankowaniu wynosi " + calculateRange() +
+                    " co wykazuje, że jest ekonomiczny i można w niego inwestować.");
+        } else if(calculateRange() > 1000 && calculateRange() < 1200) {
+            System.out.println("Pojazd " + plate + " ma " + capacity + "(t) ładowności. Zasięg na jednym tankowaniu wynosi " + calculateRange() +
+                    " co wykazuje, że jest przeciętny pod względem ekonomicznym.");
+        } else {
+            System.out.println("Pojazd " + plate + " ma " + capacity + "(t) ładowności. Zasięg na jednym tankowaniu wynosi " + calculateRange() +
+                    " co wykazuje, że nie jest ekonomiczny i należy się go pozbyć!");
+        }
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+
+    public float getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(float capacity) {
+        this.capacity = capacity;
+    }
+
+
+    public int getFuelcap() {
+        return fuelcap;
+    }
+    public void setFuelcap(int fuelcap) {
+        this.fuelcap = fuelcap;
+    }
+
+
+    public float getLkm() {
+        return lkm;
+    }
+    public void setLkm(float lkm) {
+        this.lkm = lkm;
     }
 }
--- VehicleCalculate.java
+++ VehicleCalculate.java
@@ -1,42 +1,15 @@
 package Vehicle;
 
-import Vehicle.Vehicle;
-
 public class VehicleCalculate {
     public static void main(String[] args) {
 
-        Vehicle truck1 = new Vehicle();
-        Vehicle truck2 = new Vehicle();
-        Vehicle truck3 = new Vehicle();
-        Vehicle truck4 = new Vehicle();
-        int range;
-
-        truck1.plate = "KR878NJ/MAN TGL";
-        truck1.capacity = 5.5f;
-        truck1.fuelcap = 250;
-        truck1.lkm = 19.5;
-        System.out.print("Pojazd KR878NJ ma ");
-        truck1.range();
-
-        truck2.plate = "KR9A301/MAN TGM";
-        truck2.capacity = 7.0f;
-        truck2.fuelcap = 200;
-        truck2.lkm = 23.5;
-        System.out.print("Pojazd KR9A301 ma ");
-        truck2.range();
-
-        truck3.plate = "KR3N065/DAF LF";
-        truck3.capacity = 8.0f;
-        truck3.fuelcap = 250;
-        truck3.lkm = 22.5;
-        System.out.print("Pojazd KR3N065 ma ");
-        truck3.range();
-
-        truck4.plate = "KR8CA24/IVECO EC";
-        truck4.capacity = 8.0f;
-        truck4.fuelcap = 300;
-        truck4.lkm = 21.5;
-        System.out.print("Pojazd KR8CA24 ma ");
-        truck4.range();
+        Vehicle truck1 = new Vehicle("KR878NJ/MAN TGL", 5.5f, 250, 19.5f);
+        truck1.printVehicleInfo();
+        Vehicle truck2 = new Vehicle("KR9A301/MAN TGM", 7.0f, 200, 23.5f);
+        truck2.printVehicleInfo();
+        Vehicle truck3 = new Vehicle( "KR3N065/DAF LF", 8.0f, 250, 22.5f);
+        truck3.printVehicleInfo();
+        Vehicle truck4 = new Vehicle( "KR8CA24/IVECO EC", 8.0f, 300, 21.5f);
+        truck4.printVehicleInfo();
     }
 }
